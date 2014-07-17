d0adb4cd78f0bc42a5fdcdaae5191db3ed06ea03
Tony Malagisi <mtb3000gt@gmail.com>
2014-07-16 22:04:37 -0400

    Liquify

--- liquid.mk
+++ liquid.mk
@@ -12,17 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common LiquidSmooth stuff.
+$(call inherit-product, vendor/liquid/config/common_tablet.mk)
 
 # Inherit device configuration for bowser
 $(call inherit-product, device/amazon/jem/full_jem.mk)
-$(call inherit-product, device/amazon/bowser-common/cm.mk)
+$(call inherit-product, device/amazon/bowser-common/liquid.mk)
 
 #TARGET_SCREEN_WIDTH := 1920
 TARGET_SCREEN_WIDTH := 480
 TARGET_SCREEN_HEIGHT := 1200
 
-PRODUCT_NAME := cm_jem
+PRODUCT_NAME := liquid_jem
 PRODUCT_RELEASE_NAME := KindleFireHD
 
